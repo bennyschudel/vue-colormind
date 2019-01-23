9b2ecf68d76e11f263e2522a22fce23b915802f4
Benjamin Schudel <scb@panter.ch>
2019-01-23 23:46:19 +0100

    feat: add colormind model

--- src/services/colormind.js
+++ src/services/colormind.js
@@ -1,12 +1,16 @@
-const API_ENDPOINT = 'http://colormind.io/api/';
+const API_ENDPOINT = 'http://colormind.io/';
 
 export const loadPalette = ({
   colors = [[0, 0, 0], [172, 64, 0], null, null, null],
   model = 'default',
 } = {}) => {
   return new Promise(resolve => {
-    const url = API_ENDPOINT;
-    const input = colors.map(d => (d ? d : 'N'));
+    const url = `${API_ENDPOINT}api/`;
+    let input = colors.map(d => (d ? d : 'N'));
+
+    if (input.every(v => v === 'N')) {
+      input = undefined;
+    }
 
     const data = {
       model,
@@ -24,5 +28,23 @@ export const loadPalette = ({
 
     http.open('POST', url, true);
     http.send(JSON.stringify(data));
+  });
+};
+
+export const loadAvailableModels = () => {
+  return new Promise(resolve => {
+    const url = `${API_ENDPOINT}list/`;
+
+    const http = new XMLHttpRequest();
+
+    http.onreadystatechange = function() {
+      if (http.readyState == 4 && http.status == 200) {
+        var models = JSON.parse(http.responseText).result;
+        resolve(models);
+      }
+    };
+
+    http.open('GET', url, true);
+    http.send();
   });
 };
